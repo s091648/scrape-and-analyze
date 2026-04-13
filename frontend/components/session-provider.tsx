6424d25b4720591ff95ef33d241e77e8af31d93d
s0916 <s0916@local.dev>
2026-04-14 05:00:39 +0800

    💈 [FIX] disable refetch on window focus to prevent re-render and DOM detach

--- frontend/components/session-provider.tsx
+++ frontend/components/session-provider.tsx
@@ -3,5 +3,5 @@ import { SessionProvider } from 'next-auth/react'
 import { ReactNode } from 'react'
 
 export default function SessionProviderWrapper({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>
+  return <SessionProvider refetchOnWindowFocus={false}>{children}</SessionProvider>
 }
