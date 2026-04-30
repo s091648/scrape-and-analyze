fbe6cb67e24fe10736f9ccb5eb7e23fc312a3c7a
Chiao-Yin, Teng <joy68304276@gmail.com>
2026-05-01 00:11:08 +0800

    Add BaseScraper to module exports

--- src/infrastructure/collection/scrapers/__init__.py
+++ src/infrastructure/collection/scrapers/__init__.py
@@ -1,9 +1,11 @@
+from .base_scraper import BaseScraper
 from .rss_scraper import RssScraper
 from .blog_scraper import BlogScraper
 from .arxiv_scraper import ArxivScraper
 from .scraper_factory import ConcreteScraperFactory
 
 __all__ = [
+    'BaseScraper',
     'RssScraper',
     'BlogScraper',
     'ArxivScraper',
